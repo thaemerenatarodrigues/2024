00f469f9067b2c3a336d35f4d8b64acfd5e24072
thaemerenatarodrigues <130755793+thaemerenatarodrigues@users.noreply.github.com>
2024-06-21 08:35:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ document.querySelectorAll('.botao').forEach(btn => {
         document.querySelectorAll('.aba-conteudo')[index].classList.add('ativo');
     });
 });
+
+const contadores = document.querySelectorAll(".contador");
+contadores[0].textContent = "Contagem regressiva";
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+contadores[0].textContent = tempoObjetivo1;
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual; 
+
+const botoes = document.querySelectorAll(".botao");
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");{
+                botoes[i].classList.add("ativo");
+            }
+        }
+    }
+}
